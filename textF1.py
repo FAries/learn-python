48d3944d25838fd8a72467404fd24e26f6a9e3ec
FAries <810343008@qq.com>
2015-04-10 11:04:49 +0800

    Signed-off-by: luyongxing <835697991@qq.com> Signed-off-by: FAries <810343008@qq.com>

---
+++ textF1.py
@@ -0,0 +1,17 @@
+#-*-coding:utf-8-*-
+print "                        沪杭车中                        "
+print "匆匆匆，催催催！"
+print "一卷烟，一片山，"
+print "几点云影，一道水，"
+print 
+print "一条桥，一支橹声，"
+print "一林松，一丛竹，"
+print "绿叶纷纷："
+print 
+print "艳色的田野，艳色的秋景，"
+print "梦境似得分明，"
+print "模糊，消隐——"
+print "催催催！"
+print "是车轮还是光明？"
+print "催老了秋容。"
+print "催老了人生。"
